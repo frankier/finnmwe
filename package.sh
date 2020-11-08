3fde5466ecc297badef1f4386236f9ac54b54061
Frankie Robertson <frankie@robertson.name>
2020-11-08 13:23:08 +0200

    Add datasette-json-html

--- package.sh
+++ package.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-datasette package --tag frankierr/finnmwe:latest finnmwe && \
+datasette package \
+    --install datasette-json-html \
+    --tag frankierr/finnmwe:latest \
+    finnmwe && \
   docker push frankierr/finnmwe:latest
